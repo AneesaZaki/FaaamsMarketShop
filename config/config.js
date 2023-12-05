885f43d817a7a3621ffffa1bf425e1ab6192f481
areebah-nadeem <113574448+areebah-nadeem@users.noreply.github.com>
2023-12-05 18:25:50 -0500

    Update config.js
    updated  link line 6

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const config = {
   port: 3000,
   jwtSecret: "YOUR_secret_key",
   mongoUri:
-  "mongodb+srv://Comp229G3:Comp229G3@cluster0.3tdemug.mongodb.net/onlinemarketplace?retryWrites=true&w=majority",
+  "mongodb+srv://Comp229G3:Comp229G3@centennial.ubonb0f.mongodb.net/onlinemarketplace?retryWrites=true&w=majority",
 
 };
 export default config;
