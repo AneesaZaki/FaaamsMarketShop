2c740f9aca6ce55b54919f99ed9c2d9191ee4cb9
konsman <konsman@gmail.com>
2023-12-06 16:21:44 -0500

    change

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import App from './App'
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
+
 
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
