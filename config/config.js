b7288ea4fff8e55ddf477c0f1434754658838188
Maan Patel <maanpatel1811@gmail.com>
2023-12-05 22:13:26 -0600

    updated the config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 const config = {
   env: "development",
-  port: 3000,
+  // port: 3000,
+  port: process.env.PORT || 3000,
   jwtSecret: "YOUR_secret_key",
   mongoUri:
   "mongodb+srv://Comp229G3:Comp229G3@centennial.ubonb0f.mongodb.net/onlinemarketplace?retryWrites=true&w=majority",
